0334ddba29d968374b00128350ad9d977f99cd29
patrick-kwon <patrick.kwon@webtoonscorp.com>
2022-03-13 06:07:43 +0000

    add test_e4e

--- test_e4e.py
+++ test_e4e.py
@@ -6,16 +6,19 @@ import numpy as np
 from PIL import Image
 
 import torch
-from utils.img_utils import postproc
+
+from models.psp import pSp
+from utils.img_utils import preproc, postproc
 from utils.load_utils import load_network_pkl, blend_models, blend_models_stylegan3
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("seeds", type=int, nargs='+', help="Random seed for latent code generation")
-    parser.add_argument("--pretrain_weight", type=str, default="stylegan2-pretrained.pkl", 
+    parser.add_argument("imgs", type=Path, nargs='+', help="Name of image files to be translated")
+    parser.add_argument("--pretrain_weight", type=Path, default="stylegan2-pretrained.pkl", 
         help="Pretrained StyleGAN Generator to act as high resolution layer components")
-    parser.add_argument("--finetune_weight", type=str, default="stylegan2-finetuned.pkl", 
+    parser.add_argument("--finetune_weight", type=Path, default="stylegan2-finetuned.pkl", 
         help="Finetuned StyleGAN Generator to act as low resolution layer components")
+    parser.add_argument("--e4e_ckpt", type=Path, default="lhq-e4e-encoder.pt")
     parser.add_argument("--network_type", type=str, default="stylegan2", choices=["stylegan2", "stylegan3"], 
         help="Which type of stylegan to use.")
     parser.add_argument("--blend_lv", type=int, default=8, 
@@ -25,6 +28,7 @@ if __name__ == "__main__":
 
     pretrain_path = Path("weights").joinpath(args.pretrain_weight)
     finetune_path = Path("weights").joinpath(args.finetune_weight)
+    e4e_ckpt_path = Path("weights").joinpath(args.e4e_ckpt)
 
     if args.output_path is None:
         args.output_path = Path(f"imgs/{pretrain_path.stem}_{finetune_path.stem}_{args.blend_lv}")
@@ -40,20 +44,24 @@ if __name__ == "__main__":
     else:
         G_blended = blend_models_stylegan3(G_pretrain, G_finetune, args.blend_lv)
 
-    for seed in tqdm(args.seeds):
-        z = torch.from_numpy(np.random.RandomState(seed).randn(1, 512)).to("cuda")
-        label = torch.zeros([1, G_pretrain.c_dim], device="cuda")
+    opts = torch.load(e4e_ckpt_path, map_location="cpu")["opts"]
+    opts['checkpoint_path'] = e4e_ckpt_path
+    opts['device'] = "cuda"
+    opts['model_type'] = args.network_type
+    net = pSp(argparse.Namespace(**opts)).eval().to("cuda")
+
+    for img_path in tqdm(args.imgs):
+        img = Image.open(img_path).resize((256,256))
+        img_t = preproc(img).to("cuda")
 
         with torch.no_grad():
-            ws = G_pretrain.mapping(z, label)
-
-        with torch.no_grad():
+            _, ws = net(img_t, randomize_noise=False, return_latents=True)
             img_pretrain = G_pretrain.synthesis(ws, noise_mode="const")
             img_finetune = G_finetune.synthesis(ws, noise_mode="const")
             img_blended = G_blended.synthesis(ws, noise_mode="const")
 
         final_img = np.concatenate([postproc(img_pretrain[0]), postproc(img_finetune[0]), postproc(img_blended[0])], axis=1)
-        Image.fromarray(final_img).save(args.output_path.joinpath(f"seed_{seed}.jpg"))
+        Image.fromarray(final_img).save(args.output_path.joinpath(img_path.name))
 
 
 
